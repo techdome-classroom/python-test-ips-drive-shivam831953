b754cc949f596c9a2cc37755deb9a4e6e675ad67
shivam831953 <168169754+shivam831953@users.noreply.github.com>
2024-04-26 13:37:24 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -7,7 +7,37 @@ def smallest_missing_positive_integer(nums: List[int]) -> int:
     """
     pass
 
+def solution(A): 
 
+    m = max(A)
+    if m < 1:
+
+        
+        return 1
+    if len(A) == 1:
+
+        
+        return 2 if A[0] == 1 else 1
+    l = [0] * m
+    for i in range(len(A)):
+        if A[i] > 0:
+            if l[A[i] - 1] != 1:
+
+                
+                l[A[i] - 1] = 1
+    for i in range(len(l)):
+
+        
+        if l[i] == 0:
+            return i + 1
+            
+    return i + 2
+
+
+
+if __name__ == '__main__':
+    arr = [0, 10, 2, -10, -20]
+    print(solution(arr))
 
 
 
