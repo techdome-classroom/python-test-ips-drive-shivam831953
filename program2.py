aaa3c9951ef68b1301568abfd2e40ed2e04b0c91
shivam831953 <168169754+shivam831953@users.noreply.github.com>
2024-04-26 13:45:48 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -8,6 +8,45 @@ def longest_substring(s: str) -> int:
 
     """ 
     pass
+    def areDistinct(str, i, j):
+
+    
+    visited = [0] * (256)
+
+    for k in range(i, j + 1):
+        if (visited[ord(str[k])] == True):
+            return False
+
+        visited[ord(str[k])] = True
+
+    return True
+
+
+
+def longestUniqueSubsttr(str):
+
+    n = len(str)
+
+    
+    res = 0
+
+    for i in range(n):
+        for j in range(i, n):
+            if (areDistinct(str, i, j)):
+                res = max(res, j - i + 1)
+
+    return res
+
+
+
+if __name__ == '__main__':
+
+    str = "geeksforgeeks"
+    print("The input is ", str)
+
+    len = longestUniqueSubsttr(str)
+    print("The length of the longest "
+          "non-repeating character substring is ", len)
 
 
 
